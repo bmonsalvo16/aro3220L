67fffdd435a513c20b8c5ab7ab9e943cfc75a4e2
bdchaputa <143647453+bdchaputa@users.noreply.github.com>
2023-09-13 09:25:01 -0700

    aero_param.m file
    aero_param.m file template from Canvas

---
+++ aero_param.m
@@ -0,0 +1,20 @@
+% Resistance
+Rm = 8.4;
+% Current-torque (N-m/A)
+kt = 0.042;
+% Back-emf constant (V-s/rad)
+km = 0.042;
+% Propeller Drag (N-m-s/rad)
+kd = 1*10^(-5);
+% Rotor inertia (kg-m^2)
+Jr = 4e-6;
+% Hub mass (kg)
+mh = 0.003; % 3 g
+% Hub radius (m)
+rh = 9/1000/2; % diameter 9 mm
+% Hub inertia (kg-m^2)
+Jh = 0.5*mh*rh^2;
+% Propeller moment of inertia (kg-m^2)
+Jp = 7.2*10^(-6); 
+% Equivalent moment of inertia (kg-m^2)
+Jeq = Jr + Jh + Jp;
